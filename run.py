477d194852a40f415439912c78c4ae6ec73da6c5
hainingw <hainingw@yishow.me>
2016-11-06 21:04:47 +0800

    Added tuling replay groupchat text message

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from __future__ import  unicode_literals
 
 import itchat
 from itchat.content import *
+from bot import tuling
 
 
 # auto accept friends request
@@ -17,6 +18,14 @@ def add_friend(msg):
 @itchat.msg_register(TEXT)
 def text_reply(msg):
     text = itchat.send(msg['Text'], msg['FromUserName'])
+
+
+@itchat.msg_register(TEXT, isGroupChat=True)
+def groupchat_reply(msg):
+    if msg['isAt']:
+        replay_text = tuling.replay_text(msg['Content'],
+                msg['ActualNickName']) or '系统错误'
+        itchat.send(replay_text, msg['FromUserName'])
 
 
 if __name__ == '__main__':
