94b6fa4ef72a04329659334d5cd3eb9f746035c5
hainingw <hainingw@yishow.me>
2016-11-06 20:35:48 +0800

    Added tuling robot replay text closed #7

---
+++ config.dist.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+tuling_config = {
+    'api_key': 'xxxxx',
+    '_api_secret': 'xxxxx'
+}
---
+++ utils/tuling.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+class TuLing(object):
+    """
+    Tuling robot http://www.tuling123.com
+    Doc http://www.tuling123.com/help/h_cent_webapi.jhtml
+    """
+    API_URL = 'http://www.tuling123.com/openapi/api'
+
+    SUB_TYPE_TEXT = 100000
+
+    def __init__(self, *args, **kwargs):
+        super(TuLing, self).__init__()
+        self._api_key = kwargs.get('api_key')
+        self._api_secret = kwargs.get('api_secret')
+        self._api_url = kwargs.get('api_url', TuLing.API_URL)
+        assert self._api_key and self._api_secret, \
+            'tuling key or secret is None'
+
+    def _post(self, data):
+        response = requests.post(url=self._api_url, data=data)
+        if response.status_code is not 200:
+            return None
+        return response.json()
+
+    def replay_text(self, info, userid=None):
+        data = {
+            'key': self._api_key,
+            'info': info
+        }
+        if userid: data['userid'] = userid
+        return_dict = self._post(data)
+        # TODO news and urls type
+        if return_dict['code'] == TuLing.SUB_TYPE_TEXT:
+            return return_dict['text']
+
+        return None
